431fbd0b79d76c778b171455615cbfded9aaa8ab
Luis J. Manso <luis.manso@gmail.com>
2014-06-16 13:28:51 +0200

    -

--- components/missionAgent/src/specificworker.cpp
+++ components/missionAgent/src/specificworker.cpp
@@ -16,7 +16,7 @@
  *    You should have received a copy of the GNU General Public License
  *    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
  #include "specificworker.h"
 
 /**
@@ -25,7 +25,7 @@
 
 #define BUUU printf("%s: %d\n", __FILE__, __LINE__);
 
-SpecificWorker::SpecificWorker(MapPrx& mprx) : GenericWorker(mprx)	
+SpecificWorker::SpecificWorker(MapPrx& mprx) : GenericWorker(mprx)
 {
 	refresh = false;
 	worldModel = AGMModel::SPtr(new AGMModel());
@@ -36,12 +36,18 @@ SpecificWorker::SpecificWorker(MapPrx& mprx) : GenericWorker(mprx)
 	rcdraw1 = new RCDraw(modelWidget);
 	modelWidget->setPalette(palette1);
 	modelDrawer = new AGMModelDrawer(rcdraw1, tableWidget);
-	
+
 	QPalette palette2 = targetWidget->palette();
 	palette2.setColor( backgroundRole(), QColor( 255, 255, 255 ) );
 	rcdraw2 = new RCDraw(targetWidget);
 	targetWidget->setPalette(palette2);
 	targetDrawer = new AGMModelDrawer(rcdraw2);
+
+	QTimer *secondTimer = new QTimer();
+	secondTimer->start(1000);
+	connect(secondTimer, SIGNAL(timeout()), this, SLOT(setGeometry()));
+
+	set3DViewer();
 
 	connect(quitButton,             SIGNAL(clicked()), this, SLOT(quitButtonClicked()));
 	connect(broadcastButton,        SIGNAL(clicked()), this, SLOT(broadcastButtonClicked()));
@@ -74,21 +80,6 @@ void SpecificWorker::compute( )
 		/// Print PLAN
 		QString planString;
 		planMutex.lock();
-		printf("%d\n", (int)plan.actions.size());
-		printf("%d\n", (int)plan.actions.size());
-		printf("%d\n", (int)plan.actions.size());
-		printf("%d\n", (int)plan.actions.size());
-		printf("%d\n", (int)plan.actions.size());
-		printf("%d\n", (int)plan.actions.size());
-		printf("%d\n", (int)plan.actions.size());
-		printf("%d\n", (int)plan.actions.size());
-		printf("%d\n", (int)plan.actions.size());
-		printf("%d\n", (int)plan.actions.size());
-		printf("%d\n", (int)plan.actions.size());
-		printf("%d\n", (int)plan.actions.size());
-		printf("%d\n", (int)plan.actions.size());
-		printf("%d\n", (int)plan.actions.size());
-		printf("%d\n", (int)plan.actions.size());
 		for (uint i=0; i<plan.actions.size(); ++i)
 		{
 			planString += "<p><b>" + QString::number(i+1) + "</b> ";
@@ -109,8 +100,15 @@ void SpecificWorker::compute( )
 	}
 
 	modelMutex.lock();
-	modelDrawer->update(worldModel);
-	targetDrawer->update(targetModel);
+	if (tabWidget->currentIndex()==0)
+	{
+		modelDrawer->update(worldModel);
+		targetDrawer->update(targetModel);
+	}
+	else
+	{
+		graphViewer->update(worldModel);
+	}
 	modelMutex.unlock();
 
 	taim = QTime::currentTime();
@@ -175,12 +173,12 @@ void SpecificWorker::setMissionFindMug()
 	AGMModelSymbol::SPtr object  = targetModel->newSymbol("object");
 	AGMModelSymbol::SPtr classS  = targetModel->newSymbol("class");
 	AGMModelSymbol::SPtr mugType = targetModel->newSymbol("mug");
-	
+
 	robot->identifier   = 1000;
 	object->identifier  = 1001;
 	classS->identifier  = 1002;
 	mugType->identifier = 1003;
-	
+
 	targetModel->addEdgeByIdentifiers( robot->identifier,  object->identifier, "know");
 	targetModel->addEdgeByIdentifiers(object->identifier,  classS->identifier, "prop");
 	targetModel->addEdgeByIdentifiers(classS->identifier, mugType->identifier, "prop");
@@ -263,3 +261,22 @@ void SpecificWorker::resetClicked()
 }
 
 
+void SpecificWorker::set3DViewer()
+{
+#if QT_VERSION >= 0x050000
+	// Qt5 is currently crashing and reporting "Cannot make QOpenGLContext current in a different thread" when the viewer is run multi-threaded, this is regression from Qt4
+	osgViewer::ViewerBase::ThreadingModel threadingModel = osgViewer::ViewerBase::SingleThreaded;
+#else
+	osgViewer::ViewerBase::ThreadingModel threadingModel = osgViewer::ViewerBase::CullDrawThreadPerContext;
+#endif
+
+	graphViewer = new GraphModelViewer(threadingModel, widget3D, true);
+
+	setGeometry();
+	graphViewer->show();
+}
+
+void SpecificWorker::setGeometry()
+{
+	graphViewer->setGeometry(0, 0, widget3D->width(), widget3D->height());
+}
