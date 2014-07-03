a3252c4ba1601d0725949d7bd76ef7fe229009db
pbustos <you@example.com>
2014-07-03 13:02:57 +0200

    speech interface in googleasr

---
+++ components/speechGoogleComp/src/speechI.cpp
@@ -0,0 +1,43 @@
+/*
+ *    Copyright (C) 2006-2010 by RoboLab - University of Extremadura
+ *
+ *    This file is part of RoboComp
+ *
+ *    RoboComp is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    RoboComp is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "speechI.h"
+
+SpeechI::SpeechI(GenericWorker *_worker, QObject *parent) : QObject(parent)
+{
+	worker = _worker;
+	mutex = worker->mutex;       // Shared worker mutex
+	// Component initialization...
+}
+
+
+SpeechI::~SpeechI()
+{
+	// Free component resources here
+}
+
+// Component functions, implementation
+bool SpeechI::say(const string& text, bool owerwrite, const Ice::Current&){
+	return worker->say(text,owerwrite);
+}
+
+bool SpeechI::isBusy(const Ice::Current&){
+	return worker->isBusy();
+}
+
+
