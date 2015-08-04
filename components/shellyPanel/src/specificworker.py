ae832bdab2966c4b0d6bb376076952d27afa9f1f
mhaut <juanmariohaut@unex.es>
2015-08-04 13:43:40 +0200

    thread support in shellypanel

--- components/shellyPanel/src/specificworker.py
+++ components/shellyPanel/src/specificworker.py
@@ -57,48 +57,309 @@ from RoboCompASRPublish import *
 
 from asrpublishI import *
 
+
+
+
+
+
+##############################################################################
+############		MANAGEMENT NUCS IN BACKGROUND		##############
+##############################################################################
+
+tempN1 = ''
+tempN2 = ''
+loadN1 = ''
+loadN2 = ''
+wifiN1 = ''
+wifiN2 = ''
+#on_1 = True
+#on_2 = True
+hosts = dict()
+statusN1 = "on"     # on/reboot/off/unknown
+statusN2 = "on"
+
+class MyShellyThread(QtCore.QThread):
+	def __init__(self, parent = super):
+		QtCore.QThread.__init__(self, None)
+		self.exiting = False
+		# variables for work with ssh
+		self.ssh = None
+		self.transport = None
+
+	def run(self):
+		reloj = QtCore.QTime.currentTime()
+		while True:
+			#print "thead executing"			
+			if reloj.elapsed() > 5000:
+				print "load data Nucs"
+				self.loadTemp()
+				self.loadAverage()
+				self.loadWiFiSignal()
+				reloj.restart();
+
+			global statusN1, statusN2
+			if statusN1 == "off":
+				self.shutdownNUC1()
+				statusN1 = "unknown"
+			elif statusN1 == "reboot":
+				self.resetNUC1()
+				statusN1 = "on"
+			if statusN2 == "off":
+				self.shutdownNUC2()
+				statusN2 = "unknown"
+			elif statusN2 == "reboot":
+				self.resetNUC2()
+				statusN1 = "on"
+
+
+
+
+	def loadTemp(self):
+		global statusN1, statusN2
+		if statusN1 == "on" or statusN2 == "on":
+			hostnames = ['robonuc1.local','robonuc2.local']
+			for hostname in hostnames:
+				global hosts
+				self.connect(hostname,"robolab",hosts[hostname])
+				output=self.runcmd('sensors')
+				if not "ERROR" in output:
+					output = output.split('\n')
+					temC1 = (output[8].split('         ')[1].split('  ')[0])[1:-3]
+					temC2 = (output[9].split('         ')[1].split('  ')[0])[1:-3]
+					global tempN1, tempN2
+					if hostname == 'robonuc1.local':					
+						#self.ui.temperatura1.setText('Temp: '+ str((float(temC1) + float(temC2))/float(2.0)))
+						#global tempN1
+						tempN1 = 'Temp: '+ str((float(temC1) + float(temC2))/float(2.0))
+					else:
+						#self.ui.temperatura2.setText('Temp: '+ str((float(temC1) + float(temC2))/float(2.0)))
+						#global tempN2
+						tempN2 = 'Temp: '+ str((float(temC1) + float(temC2))/float(2.0))
+					self.disconnect()
+				else:
+					print 'ERROR with: '+str(hostname)
+					#global tempN1, tempN2
+					tempN1 = tempN2 = "ERROR"
+		else:
+			print "not nucs working"
+
+	def loadAverage(self):
+		global statusN1, statusN2
+		if statusN1 == "on" and statusN2 == "on":
+			maxAttemp = 50
+			hostnames = ['robonuc1.local','robonuc2.local']
+			for hostname in hostnames:
+				global hosts
+				self.connect(hostname,"robolab",hosts[hostname])
+				output=self.runcmd('top -bn '+str(maxAttemp)+' -d 0.01 | grep \'%Cpu(s)\' | gawk \'{print $2+$4+$6}\'')
+				#if output:
+				if not "ERROR" in output:
+					output = output.split('\n')
+					aux = 0
+					cont = 0
+					for load in output:
+						if load != '':
+							aux += float(load[:-1])
+							cont += 1
+					aux = aux / maxAttemp
+					global loadN1, loadN2
+					if hostname == 'robonuc1.local':
+						#self.ui.loadavg1.setText('Load: '+str(aux))
+						#global loadN1
+						loadN1 = 'Load: '+str(aux)
+					else:
+						#self.ui.loadavg2.setText('Load: '+str(aux))
+						#global loadN2
+						loadN2 = 'Load: '+str(aux)
+				else:
+					print 'ERROR with: '+str(hostname)				
+					loadN1 = loadN2 = "ERROR"
+		else:
+			print "not nucs working"
+
+	def loadWiFiSignal(self):
+		global statusN1, statusN2
+		if statusN1 == "on" and statusN2 == "on":
+			ifaceN1 = 'wlan2'
+			ifaceN2 = 'wlan3'
+			hostnames = ['robonuc1.local','robonuc2.local']
+			for hostname in hostnames:
+				global hosts
+				self.connect(hostname,"robolab",hosts[hostname])
+				if hostname == 'robonuc1.local':
+					output=self.runcmd('iwconfig '+ifaceN1+' | grep -e "Signal level"')
+				else:
+					output=self.runcmd('iwconfig '+ifaceN2+' | grep -e "Signal level"')
+				#if output:
+				if not "ERROR" in output:
+					signal = 'Wifi: '+((output.split('='))[2])
+					global wifiN1, wifiN2
+					if hostname == 'robonuc1.local':
+						#self.ui.loadwifi1.setText(signal)
+						#print 'Wifi NUC1',signal
+						wifiN1 = 'WifiN1: '+str(signal)
+					else:
+						#self.ui.loadwifi2.setText(signal)
+						#print 'Wifi NUC2',signal
+						wifiN2 = 'WifiN2: '+str(signal)
+				else:
+					print 'ERROR with: '+str(hostname)
+					wifiN1 = wifiN2 = "ERROR"
+		else:
+			print "not nucs working"
+
+	def disconnect (self):
+		if self.transport is not None:
+			self.transport.close()
+		if self.ssh is not None:
+			self.ssh.close()
+
+	def connect(self,hostname,username,password,port=22):
+		self.hostname = hostname
+		self.username = username
+		self.password = password
+
+		self.ssh = paramiko.SSHClient()
+		#Don't use host key auto add policy for production servers
+		self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+		self.ssh.load_system_host_keys()
+		try:
+			self.ssh.connect(hostname,port,username,password)
+			self.transport=self.ssh.get_transport()
+		except (socket.error,paramiko.AuthenticationException) as message:
+			print "ERROR: SSH connection to "+self.hostname+" failed: " +str(message)
+			#sys.exit(1)
+		return  self.transport is not None
+
+	def runcmd(self,cmd,sudoenabled=True):
+		if sudoenabled:
+			fullcmd="echo " + self.password + " |   sudo -S -p '' " + cmd
+		else:
+			fullcmd=cmd
+		if self.transport is None:
+			return "ERROR: connection was not established"
+		session=self.transport.open_session()
+		session.set_combine_stderr(True)
+		if sudoenabled:
+			session.get_pty()
+		session.exec_command(fullcmd)
+		stdout = session.makefile('rb', -1)
+		output=stdout.read()
+		session.close()
+		return output
+
+	@QtCore.Slot()
+	def shutdownNUC1(self):
+		print "POWEROFF NUC1"
+		hostname = 'robonuc1.local'
+		i = 3
+		global hosts
+		while i > 0:
+			self.connect(hostname,"robolab",hosts[hostname])
+			output=self.runcmd('sudo poweroff')
+			if output:
+				print output
+				self.disconnect()
+				#global on_1
+				#on_1 = False
+				break
+			else:
+				i -= 1
+				print 'ERROR shutdown NUC1. Trying number '+str(i)
+
+	@QtCore.Slot()
+	def resetNUC1(self):
+		global hosts
+		print "REBOOT NUC1"
+		hostname = 'robonuc1.local'
+		i = 3
+		while i > 0:
+			self.connect(hostname,"robolab",hosts[hostname])
+			output=self.runcmd('sudo reboot')
+			if output:
+				print output
+				self.disconnect()
+				break
+			else:
+				i -= 1
+				print ' Error reset NUC1.  Trying number '+str(i)
+
+	@QtCore.Slot()
+	def shutdownNUC2(self):
+		print "POWEROFF NUC2"
+		hostname = 'robonuc2.local'
+		i = 3
+		global hosts
+		while i > 0:
+			self.connect(hostname,"robolab",hosts[hostname])
+			output=self.runcmd('sudo poweroff')
+			if output:
+				print output
+				self.disconnect()
+				#global on_2
+				#on_2 = False
+				break;
+			else:
+				i -= 1
+				print 'ERROR shutdown NUC2.  Trying number '+str(i)
+
+	@QtCore.Slot()
+	def resetNUC2(self):
+		global hosts
+		print "REBOOT NUC2"
+		hostname = 'robonuc2.local'
+		i = 3
+		while i > 0:
+			self.connect(hostname,"robolab",hosts[hostname])
+			output=self.runcmd('sudo reboot')
+			if output:
+				print output
+	                        self.disconnect()
+				break
+			else:
+				i -= 1
+				print 'ERROR reset NUC1. Trying number '+str(i)
+
+
+
+####################################################################################################
+####################################################################################################
+####################################################################################################
+
+
 class SpecificWorker(GenericWorker):
 	def __init__(self, proxy_map):
 		super(SpecificWorker, self).__init__(proxy_map)
 		self.timer.timeout.connect(self.compute)
 		self.Period = 2000
 		self.timer.start(self.Period)
-	
-		# timer for the get informations of nucs
-		timer2 = QtCore.QTimer(self)		
-		timer2.timeout.connect(self.computeTemp)
-		timer2.start(10000)
-		timer3 = QtCore.QTimer(self)
-		timer3.timeout.connect(self.computeLoad)
-		timer3.start(15000)
-		timer4 = QtCore.QTimer(self)
-		timer4.timeout.connect(self.computeWifi)
-		timer4.start(5000)
 		
 		self.ui.navigationButton.clicked.connect(self.goNavigation)
 		self.ui.ikButton.clicked.connect(self.goIK)
 		self.ui.vikButton.clicked.connect(self.goVIK)
 		self.ui.lowButton.clicked.connect(self.lowPosition)
 		self.ui.topButton.clicked.connect(self.topPosition)
-		self.ui.shutdown1Button.clicked.connect(self.shutdownNUC1)
-		self.ui.shutdown2Button.clicked.connect(self.shutdownNUC2)
-		self.ui.reset1Button.clicked.connect(self.resetNUC1)
-		self.ui.reset2Button.clicked.connect(self.resetNUC2)
-		
-		self.on_1 = True
-		self.on_2 = True
-		
+		self.ui.shutdown1Button.clicked.connect(self.putOffN1)
+		self.ui.shutdown2Button.clicked.connect(self.putOffN2)
+		self.ui.reset1Button.clicked.connect(self.putRebootN1)
+		self.ui.reset2Button.clicked.connect(self.putRebootN2)
+	
 		# variables for work with ssh
-		self.ssh = None
-		self.transport = None
-		self.hosts = dict()
+		#self.ssh = None
+		#self.transport = None
+		#self.hosts = dict()
+		
+		self.thread = MyShellyThread()
+		#workerThread = new WorkerThread(this);
+		self.thread.start();
 		try:
 			for line in open(os.getenv("HOME")+'/.rcremote', 'r').readlines():
 				try:
 					s = line.strip().split('#', 1)
 					if len(s) < 2:
 						continue
-					self.hosts[s[0]] = s[1]
+					global hosts
+					hosts[s[0]] = s[1]
 				except:
 					print ("can't find password for "+str(s[0])+" in ~/.rcremote  (format 'host#password')")
 		except:
@@ -106,43 +367,48 @@ class SpecificWorker(GenericWorker):
 
 
 	def setParams(self, params):
-		#try:
-		#	par = params["InnerModelPath"]
-		#	innermodel_path=par.value
-		#	innermodel = InnerModel(innermodel_path)
-		#except:
-		#	traceback.print_exc()
-		#	print "Error reading config params"
 		return True
 
 	@QtCore.Slot()
 	def compute(self):
 		print 'SpecificWorker.compute...'
-		#try:
-		#	differentialrobot_proxy.setSpeed(100, 0)
-		#except Ice.Exception, e:
-		#	traceback.print_exc()
-		#	print e		
-		#return True
-		
+		global tempN1, tempN2,loadN1,loadN2, wifiN1, wifiN2
+		if tempN1 != '':
+			self.ui.temperatura1.setText(tempN1)
+			tempN1 = ''		
+		if tempN2 != '':
+			self.ui.temperatura2.setText(tempN2)
+			tempN2 = ''		
+		if loadN1 != '':
+			self.ui.loadavg1.setText(loadN1)
+			loadN1 = ''
+		if loadN2 != '':
+			self.ui.loadavg2.setText(loadN2)
+			loadN2 = ''
+		if wifiN1 != '':
+			self.ui.wifi1.setText(wifiN1)
+			wifiN1 = ''
+		if wifiN2 != '':
+			self.ui.wifi2.setText(wifiN2)
+			wifiN2 = ''
 
-	def computeTemp(self):
-		if self.on_1 is True and self.on_2 is True:
-			self.loadTemp()
-		else:
-			pass
 
-	def computeLoad(self):
-		if self.on_1 is True and self.on_2 is True:
-			self.loadAverage()
-		else:
-			pass
-
-	def computeWifi(self):
-		if self.on_1 is True and self.on_2 is True:
-			self.loadWiFiSignal()
-		else:
-			pass
+########################################################################
+#### MANAGEMENT
+# TODO IF NUC NOT INIT ON INIT SHELLY PANEL
+	def putOffN1(self):
+		global statusN1
+		statusN1 = "off"
+	def putOffN2(self):
+		global statusN2
+		statusN2 = "off"
+	def putRebootN1(self):
+		global statusN1
+		statusN1 = "reboot"
+	def putRebootN2(self):
+		global statusN2
+		statusN2 = "reboot"
+#################################################################################
 
 
 	#
@@ -155,70 +421,6 @@ class SpecificWorker(GenericWorker):
 		pass
 
 
-	def loadTemp(self):
-		hostnames = ['robonuc1.local','robonuc2.local']
-		for hostname in hostnames:
-			self.connect(hostname,"robolab",self.hosts[hostname])
-			output=self.runcmd('sensors')
-			if output:
-				output = output.split('\n')
-				temC1 = (output[8].split('         ')[1].split('  ')[0])[1:-3]
-				temC2 = (output[9].split('         ')[1].split('  ')[0])[1:-3]
-				if hostname == 'robonuc1.local':					
-					self.ui.temperatura1.setText('Temp: '+ str((float(temC1) + float(temC2))/float(2.0)))
-				else:
-					self.ui.temperatura2.setText('Temp: '+ str((float(temC1) + float(temC2))/float(2.0)))
-				self.disconnect()
-			else:
-				print 'ERROR with: '+str(hostname)
-
-
-
-	def loadAverage(self):
-		maxAttemp = 50
-		hostnames = ['robonuc1.local','robonuc2.local']
-		for hostname in hostnames:
-			self.connect(hostname,"robolab",self.hosts[hostname])
-			output=self.runcmd('top -bn '+str(maxAttemp)+' -d 0.01 | grep \'%Cpu(s)\' | gawk \'{print $2+$4+$6}\'')
-			if output:
-				output = output.split('\n')
-				aux = 0
-				cont = 0
-				for load in output:
-					if load != '':
-						aux += float(load[:-1])
-						cont += 1
-				aux = aux / maxAttemp
-				if hostname == 'robonuc1.local':
-					self.ui.loadavg1.setText('Load: '+str(aux))
-				else:
-					self.ui.loadavg2.setText('Load: '+str(aux))
-			else:
-				print 'ERROR with: '+str(hostname)
-
-
-
-	def loadWiFiSignal(self):
-		ifaceN1 = 'wlan2'
-		ifaceN2 = 'wlan3'
-		hostnames = ['robonuc1.local','robonuc2.local']
-		for hostname in hostnames:
-			self.connect(hostname,"robolab",self.hosts[hostname])
-			if hostname == 'robonuc1.local':
-				output=self.runcmd('iwconfig '+ifaceN1+' | grep -e "Signal level"')
-			else:
-				output=self.runcmd('iwconfig '+ifaceN2+' | grep -e "Signal level"')
-			if output:
-				signal = 'Wifi: '+((output.split('='))[2])
-
-				if hostname == 'robonuc1.local':
-					#self.ui.loadwifi1.setText(signal)
-					print 'Wifi NUC1',signal
-				else:
-					#self.ui.loadwifi2.setText(signal)
-					print 'Wifi NUC2',signal
-			else:
-				print 'ERROR with: '+str(hostname)			
 ######################################################################	
 #### NAVEGACION
 	@QtCore.Slot()
@@ -336,118 +538,3 @@ class SpecificWorker(GenericWorker):
 			#identificador = self.inversekinematics_proxy.setTargetPose6D(part,pose6D, weights)
 		#except RoboCompInverseKinematics.IKException, e:
 			#print "Expection in tester (sendPose): ", e
-
-######################################################################	
-#### OTRAS COSAS			
-
-###############################
-###			management nucs
-###############################
-	def disconnect (self):
-		if self.transport is not None:
-			self.transport.close()
-		if self.ssh is not None:
-			self.ssh.close()
-
-	def connect(self,hostname,username,password,port=22):
-		self.hostname = hostname
-		self.username = username
-		self.password = password
-
-		self.ssh = paramiko.SSHClient()
-		#Don't use host key auto add policy for production servers
-		self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-		self.ssh.load_system_host_keys()
-		try:
-			self.ssh.connect(hostname,port,username,password)
-			self.transport=self.ssh.get_transport()
-		except (socket.error,paramiko.AuthenticationException) as message:
-			print "ERROR: SSH connection to "+self.hostname+" failed: " +str(message)
-			#sys.exit(1)
-		return  self.transport is not None
-
-	def runcmd(self,cmd,sudoenabled=True):
-		if sudoenabled:
-			fullcmd="echo " + self.password + " |   sudo -S -p '' " + cmd
-		else:
-			fullcmd=cmd
-		if self.transport is None:
-			return "ERROR: connection was not established"
-		session=self.transport.open_session()
-		session.set_combine_stderr(True)
-		if sudoenabled:
-			session.get_pty()
-		session.exec_command(fullcmd)
-		stdout = session.makefile('rb', -1)
-		output=stdout.read()
-		session.close()
-		return output
-
-	@QtCore.Slot()
-	def shutdownNUC1(self):
-		print "POWEROFF NUC1"
-		hostname = 'robonuc1.local'
-		i = 3
-		while i > 0:
-			self.connect(hostname,"robolab",self.hosts[hostname])
-			output=self.runcmd('sudo poweroff')
-			if output:
-				print output
-				self.disconnect()
-				self.on_1 = False
-				break
-			else:
-				i -= 1
-				print 'ERROR shutdown NUC1. Trying number '+str(i)
-
-	@QtCore.Slot()
-	def resetNUC1(self):
-		print "REBOOT NUC1"
-		hostname = 'robonuc1.local'
-		i = 3
-		while i > 0:
-			self.connect(hostname,"robolab",self.hosts[hostname])
-			output=self.runcmd('sudo reboot')
-			if output:
-				print output
-				self.disconnect()
-				break
-			else:
-				i -= 1
-				print ' Error reset NUC1.  Trying number '+str(i)
-
-	@QtCore.Slot()
-	def shutdownNUC2(self):
-		print "POWEROFF NUC2"
-		hostname = 'robonuc2.local'
-		i = 3
-		while i > 0:
-			self.connect(hostname,"robolab",self.hosts[hostname])
-			output=self.runcmd('sudo poweroff')
-			if output:
-				print output
-				self.disconnect()
-				self.on_2 = False
-				break;
-			else:
-				i -= 1
-				print 'ERROR shutdown NUC2.  Trying number '+str(i)
-
-	@QtCore.Slot()
-	def resetNUC2(self):
-		print "REBOOT NUC2"
-		hostname = 'robonuc2.local'
-		i = 3
-		while i > 0:
-			self.connect(hostname,"robolab",self.hosts[hostname])
-			output=self.runcmd('sudo reboot')
-			if output:
-				print output
-	                        self.disconnect()
-				break
-			else:
-				i -= 1
-				print 'ERROR reset NUC1. Trying number '+str(i)
-
-
-###################################################################################################
